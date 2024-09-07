e22341f5f673312fbe9001c5500b301624439bc4
ADIRTTA <madman4205666@gmail.com>
2024-09-07 21:16:04 +0600

    Update Ipadi.py
    My tool

--- Ipadi.py
+++ Ipadi.py
@@ -1,25 +1,39 @@
 import subprocess
 import sys
 
+def print_ascii_art():
+    print("""
+   _ _    ____ ___ ____ _____ _____  _
+   / \  |  _ \_ _|  _ \_   _|_   _|/ \
+  / _ \ | | | | || |_) || |   | | / _ \
+ / ___ \| |_| | ||  _ < | |   | |/ ___ \
+/_/   \_\____/___|_| \_\|_|   |_/_/   \_\
+""")
+
 def scan_network(target):
     # Running nmap as a subprocess and capturing the output
     try:
-        print(f"Scanning {target} ...")
-        result = subprocess.run(['nmap', '-p-', target], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print("Scanning...")
+        result = subprocess.run(['nmap', '-T5', '-p-', target], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         
-        # Check if nmap ran successfully
+        # Print nmap's output
         if result.returncode == 0:
-            print(result.stdout)  # Print nmap's output
+            print(result.stdout)
         else:
-            print(f"Error occurred: {result.stderr}")  # Print any error that occurred
+            print(f"Error occurred: {result.stderr}")
+
+        # Show the exit code
+        print(f"nmap exit code: {result.returncode}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
 def main():
+    print_ascii_art()
+    
     # Check if user provided target as a command-line argument
     if len(sys.argv) != 2:
-        print("Usage: python adi.py <target>")
+        print("Usage: python network_scan.py <target>")
         sys.exit(1)
     
     # Get the target from the command-line argument
