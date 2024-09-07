e8991c11c232e0106c98e4680efb16a208a0fca8
ADIRTTA <madman4205666@gmail.com>
2024-09-07 20:48:39 +0600

    Create Ipadi.py
    My second tool

---
+++ Ipadi.py
@@ -0,0 +1,34 @@
+import subprocess
+import sys
+
+def scan_network(target):
+    # Running nmap as a subprocess and capturing the output
+    try:
+        print(f"Scanning {target} ...")
+        result = subprocess.run(['nmap', '-p-', target], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        # Check if nmap ran successfully
+        if result.returncode == 0:
+            print(result.stdout)  # Print nmap's output
+        else:
+            print(f"Error occurred: {result.stderr}")  # Print any error that occurred
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def main():
+    # Check if user provided target as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python adi.py <target>")
+        sys.exit(1)
+    
+    # Get the target from the command-line argument
+    target = sys.argv[1]
+    
+    # Perform network scan
+    scan_network(target)
+
+if __name__ == "__main__":
+    main()
+
+ print(f"Coding by adirtta")
