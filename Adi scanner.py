d698df1158b5446661885a59c6aed9b890cc4a6b
ADIRTTA <madman4205666@gmail.com>
2024-09-08 20:35:41 +0600

    Update and rename Ipadi.py to Adi scanner.py

--- Adi scanner.py
+++ Adi scanner.py
@@ -6,21 +6,15 @@ def print_ascii_art():
     # ANSI escape codes for colors
     GREEN = '\033[92m'
     RESET = '\033[0m'
-    
+
     ascii_art = f"""
-{GREEN} 
-  _   _            _    __     __  _   _ 
- | | | | ___   ___| |_  \ \   / / | |_(_)
- | |_| |/ _ \ / __| __|  \ \ / /  | __| |
- |  _  |  __/ \__ \ |_    \ V /   | |_| |
- |_| |_|\___| |___/\__|    \_/     \__|_|
-                                         
-  ____                _   
- / ___|_ __ ___  __ _| |_ 
-| |  _| '__/ _ \/ _` | __|
-| |_| | | |  __/ (_| | |_ 
- \____|_|  \___|\__,_|\__|
-                             code by adirtta 
+{GREEN}
+
+             ___    ____  ________  _______________
+            /   |  / __ \/  _/ __ \/_  __/_  __/   |
+           / /| | / / / // // /_/ / / /   / / / /| |       code by adirtta 😎
+          / ___ |/ /_/ // // _, _/ / /   / / / ___ |      THANK YOU FOR USE MY TOOL❤
+         /_/  |_/_____/___/_/ |_| /_/   /_/ /_/  |_|       don't copy my too🤗
 
 {RESET}
 """
@@ -32,16 +26,16 @@ def scan_network(target):
         print("Scanning...")
         # Start time for performance measurement
         start_time = time.time()
-        
+
         # Using nmap for a fast scan
         # -T4: Set timing template to speed up scanning
         # -p1-65535: Scan all ports
         result = subprocess.run(['nmap', '-T4', '-p1-65535', target], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        
+
         # End time for performance measurement
         end_time = time.time()
         elapsed_time = end_time - start_time
-        
+
         # Print nmap's output
         if result.returncode == 0:
             print(result.stdout)
@@ -57,15 +51,15 @@ def scan_network(target):
 
 def main():
     print_ascii_art()
-    
+
     # Check if user provided target as a command-line argument
     if len(sys.argv) != 2:
         print("Usage: python network_scan.py <target>")
         sys.exit(1)
-    
+
     # Get the target from the command-line argument
     target = sys.argv[1]
-    
+
     # Perform network scan
     scan_network(target)
 
